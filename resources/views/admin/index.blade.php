6548838323bb6fc927e83fa9807f3913ca0bb183
Soy-Zetta <mario.gaviria.mt@usa.edu.co>
2023-05-19 11:37:15 -0500

    prueba composer

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Zerotonina</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administraccion</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
