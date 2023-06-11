0ec1633711a303fdb1d8de36cb49c64831b9f6fe
Soy-Zetta <mario.gaviria.mt@usa.edu.co>
2023-06-11 17:39:58 -0500

    se corrigio el problema de crear categoria comentando la linea 20 de la tabla migrada

--- ProyectoEcommerceSena/app/Providers/AppServiceProvider.php
+++ ProyectoEcommerceSena/app/Providers/AppServiceProvider.php
@@ -3,12 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
-=======
 use Illuminate\Pagination\Paginator;
->>>>>>> 991155c35b4784bacbe55aac2b6d045ff596b0c4
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,15 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< HEAD
         Schema::defaultStringLength(191);
 
         // Habilitar timestamps en todos los modelos
         Model::unguard();
         Model::reguard();
     
-=======
         Paginator::useBootstrap();
->>>>>>> 991155c35b4784bacbe55aac2b6d045ff596b0c4
     }
 }
