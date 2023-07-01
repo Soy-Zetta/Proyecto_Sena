6b4b0226e60923ada0c7af9bcbec20daf01d736d
manuel26vm <vmburbano9@misena.edu.co>
2023-06-30 22:22:15 -0500

    agregue pregunta antes de eliminar un producto o categoria

--- ProyectoEcommerceSena/resources/views/admin/Products/index.blade.php
+++ ProyectoEcommerceSena/resources/views/admin/Products/index.blade.php
@@ -1,6 +1,5 @@
 
 
-
 @extends('adminlte::page')
 
 @section('title', 'AdminLTE')
@@ -10,6 +9,131 @@
 @stop
 
 @section('content')
+
+@if (session('delete'))
+    <div class="alert alert-success">
+        {{ session('delete') }}
+    </div>
+@endif
+
+<a href="{{ route('products.create') }}" class="btn btn-primary">Crear producto</a>
+<br><br>
+
+<form action="{{ route('buscador.search') }}" method="GET" class="mb-3">
+    @csrf
+    <div class="input-group">
+        <input type="text" class="form-control" name="buscador" placeholder="Buscar por Código de producto" required>
+        <button type="submit" class="btn btn-primary">Buscar</button>
+    </div>
+</form>
+
+<br><br>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Código</th>
+                <th>Precio</th>
+                <th>Existencias</th>
+                <th>Disponibilidad</th>
+                <th>Imagen</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($productos as $producto)
+                <tr>
+                    <td>{{ $producto->nombre }}</td>
+                    <td>{{ $producto->descripcion }}</td>
+                    <td>{{ $producto->codigo }}</td>
+                    <td>{{ $producto->precio }}</td>
+                    <td>{{ $producto->existencias }}</td>
+                    <td>
+                        @if ($producto->disponible)
+                            Disponible
+                        @else
+                            No disponible
+                        @endif
+                    </td>
+                    <td>
+                        <img src="{{ asset('images/' . $producto->imagen) }}" alt="{{ $producto->nombre }}" width="100" height="100">
+                    </td>
+                    <td>
+                        <div class="d-flex">
+                            <a href="{{ route('products.edit', $producto->id) }}" class="btn btn-primary btn-sm mr-2">Editar</a>
+
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDeleteModal{{ $producto->id }}">Eliminar</button>
+
+                            <!-- Modal de confirmación de eliminación -->
+                            <div class="modal fade" id="confirmDeleteModal{{ $producto->id }}" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel{{ $producto->id }}" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="confirmDeleteModalLabel{{ $producto->id }}">Confirmar eliminación</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ¿Estás seguro de que deseas eliminar este producto?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                            <form action="{{ route('products.destroy', $producto->id) }}" method="POST"
+                                                <form action="{{ route('products.destroy', $producto->id) }}" method="POST" id="deleteForm{{ $producto->id }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    
+    @stop
+    
+    
+    @section('css')
+        <link rel="stylesheet" href="{{ asset('css/products.css') }}">
+    @stop
+          
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- 
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Productos</h1>
+@stop
+
+@section('content')
+
+@if (session('delete'))
+    <div class="alert alert-success">
+        {{ session('delete') }}
+    </div>
+@endif
 
 <a href="{{ route('products.create') }}" class="btn btn-primary">Crear producto</a>
 <br><br>
@@ -62,7 +186,7 @@
                             <form action="{{ route('products.destroy', $producto->id) }}" method="POST" id="deleteForm">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                <button type="submit" class="btn btn-danger btn-sm" >Eliminar</button>
                             </form>
                         </div>
                     </td>
@@ -87,61 +211,4 @@
 
 
 
-
-{{-- @extends('adminlte::page')
-
-@section('title', 'AdminLTE')
-
-@section('content_header')
-    <h1 class="m-0 text-dark">Productos</h1>
-@stop
-
-@section('content')
-
-<a href="{{route('products.create')}}" class="btn btn-primary">Crear producto</a>
-<br><br>
-
-<form action="{{ route('buscador.search') }}" method="GET">
-    <fieldset enable>
-        @csrf
-        <input name ="buscador" type="text" id="disabledTextInput" class="form-control" placeholder="Buscar por Codigo de producto">
-      </div>
-      <div class="mb-3">
-      </div>
-      <button type="submit" class="btn btn-primary">Buscar</button>
-    </fieldset>
-  </form>
-
-<br><br>
-<div class="container">
-    <div class="row">   
-        @foreach ($productos as $producto)
-            <div class="col-md-4">
-                <div class="card" style="width: 20rem">
-                    <img src="{{asset('images/'.$producto->imagen)}}" alt="{{$producto->nombre}}">     
-                    <div class="card-body">
-                        <p class="card-text">{{$producto->nombre}}</p>
-                        <p class="card-text">{{$producto->precio}}</p>
-                        <p class="card-text">Existencias: {{$producto->existencias}}</p>
-                    </div>
-                    <div class="card-footer d-flex justify-content-between">
-                        <a href="{{route('products.edit',$producto->id)}}" class="btn btn-primary btn-sm">Editar</a>
-
-                        <form action="{{route('products.destroy',$producto->id)}}" method="POST" id="deleteForm">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div>
-</div>
-
-@stop
-
-
-@section('css')
-    <link rel="stylesheet" href={{ asset('css/products.css') }}>
-@stop --}}
+ --}}
--- ProyectoEcommerceSena/resources/views/admin/categories/index.blade.php
+++ ProyectoEcommerceSena/resources/views/admin/categories/index.blade.php
@@ -1,3 +1,99 @@
+
+
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Categorías</h1>
+@stop
+
+@section('content')
+ 
+<a href="{{ route('categories.create') }}" class="btn btn-primary">Crear categoría</a>
+<br><br>
+<form action="{{ route('buscador.searchCategories') }}" method="GET">
+    @csrf
+    <div class="input-group">
+        <input type="text" class="form-control" name="buscador" placeholder="Buscar por Código de producto" required>
+        <button type="submit" class="btn btn-primary">Buscar</button>
+    </div>
+</form>
+
+<form action="{{ route('categories.store') }}" method="POST">
+    @csrf
+    <!-- Aquí puedes agregar los campos y el botón para agregar una nueva categoría -->
+</form>
+
+<div>
+    <table class="table table-striped table-dark mt-5">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categorias as $categoria)
+                <tr>
+                    <td>{{ $categoria->id }}</td>
+                    <td>{{ $categoria->nombre }}</td>
+                    <td>{{ $categoria->descripcion }}</td>
+                    <td>
+                        <a href="{{ route('categories.edit', $categoria->id) }}" class="btn btn-primary">Editar</a>
+
+                        <!-- Modal de confirmación de eliminación -->
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#confirmDeleteModal{{ $categoria->id }}">
+                            Eliminar
+                        </button>
+
+                        <!-- Modal de confirmación de eliminación -->
+                        <div class="modal fade" id="confirmDeleteModal{{ $categoria->id }}" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel{{ $categoria->id }}" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="confirmDeleteModalLabel{{ $categoria->id }}">Confirmar Eliminación</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p class="text-dark">¿Estás seguro de que deseas eliminar esta categoría?</p> <!-- Agrega la clase "text-dark" -->
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                        <form action="{{ route('categories.destroy', $categoria->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Eliminar</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@stop
+
+@section('css')
+   
+@stop
+
+
+
+
+
+
+{{-- 
 @extends('adminlte::page')
 
 @section('title', 'AdminLTE')
@@ -50,11 +146,11 @@
                 </tr>
             @endforeach
         </tbody>
-    </table>
+    </table> --}}
     {{-- <div class = "d-flex justify-content-end">
         {!! $categorias->links() !!}
     </div> --}}
-</div>
+{{-- </div>
   
 
 
@@ -63,5 +159,5 @@
 
 @section('css')
    
-@stop
+@stop --}}
 
