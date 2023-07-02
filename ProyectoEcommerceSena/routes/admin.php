691b9f8cc1d600e08a27f4140356a18f8449336f
Soy-Zetta <mario.gaviria.mt@usa.edu.co>
2023-07-02 00:26:37 -0500

    organice las rutas del admin.php y agregué comentarios mas explicitos del proceso

--- ProyectoEcommerceSena/routes/admin.php
+++ ProyectoEcommerceSena/routes/admin.php
@@ -10,56 +10,55 @@ use App\Http\Controllers\Admin\BuscadorController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\ClienteController;
 use App\Http\Controllers\Admin\PedidoController;
+use Spatie\Permission\Middlewares\RoleMiddleware;
 use Illuminate\Support\Facades\Auth;
 
 
-
-
-
-
-// // // Rutas para administradores (127.0.0.1:8000/admin)
-// Route::middleware(['auth', 'role:Admin'])->prefix('admin')->group(function () {
-// // Aquí agregas todas las rutas que solo serán accesibles por los administradores
-
 Route::get('/', [HomeController::class, 'index'])->name('admin.home');
 
-//rutas crud usuarios panel de administracion
-Route::resource('users', UserController::class)->names('admin.users');
+// Rutas protegidas para el rol 'Admin' y solo serán accesibles por usuarios con el rol 'Admin' debido al middleware RoleMiddleware.
+// Aquí agregas todas las rutas que solo serán accesibles por los administradores.
+Route::middleware([RoleMiddleware::class.':Admin'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('admin.home');
+    //Panel de administración, Ruta CRUD de Usuarios 
+    Route::resource('users', UserController::class)->names('admin.users');
+    //Panel de administración, Ruta CRUD de Roles de Usuarios
+    Route::resource('roles', RoleController::class)->names('admin.roles')->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+    //Panel de administración, Ruta CRUD de los Productos.
+    Route::resource('/products', ProductoController::class)->names('products');
+    //Panel de administración, Ruta CRUD de los Proveedores.
+    Route::resource('/proveedores', ProveedorController::class)->names('proveedores');
+    //Panel de administración, Ruta de las Categorias.
+    Route::resource('/categories', categoriaController::class)->names('categories');
+    //Panel de administración, Ruta CRUD de Clientes
+    Route::resource('customers', ClienteController::class)->names('customers');
+    //Panel de administración, Ruta CRUD de Pedidos realizados por los Clientes.
+    Route::resource('pedidos', PedidoController::class)->names('pedidos');
+    //Panel de administración, Ruta Buscador.
+    Route::get('/buscador', [BuscadorController::class, 'search'])->name('buscador.search');
+    //buscador de productos panel de Categorias
+    Route::get('/buscador/categories', [BuscadorController::class, 'searchCategories'])->name('buscador.searchCategories');
+    //Buscador de productos panel de Proveedores
+    Route::get('/buscador_proveedores', [BuscadorController::class, 'searchproveedor'])->name('buscador.searchproveedor');
+    //Buscador de productos panel de Clientes
+    Route::get('/buscador_clientes', [BuscadorController::class, 'searchclientes'])->name('buscador.searchclientes');
+    //clientes crud
+    Route::resource('/clientes', ClienteController::class)->names('clientes');
+});
 
-//ruta CRUD roles panel administrativo
-Route::resource('roles', RoleController::class)->names('admin.roles')->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-
-//rutas crud proveedores panel de administracion
-Route::resource('customers', ClienteController::class)->names('customers');
-
-//rutas crud proveedores panel de administracion
-Route::resource('pedidos', PedidoController::class)->names('pedidos');
-
-
-//rutas crud proveedores panel de administracion
-Route::resource('/proveedores', ProveedorController::class)->names('proveedores');
-
-//rutas crud productos panel de administracion
-Route::resource('/products', ProductoController::class)->names('products')->middleware('auth');
 
 
 
 
-//buscador de productos,proveedores y categorias
-Route::get('/buscador', [BuscadorController::class, 'search'])->name('buscador.search');
-
-Route::get('/buscador/categories', [BuscadorController::class, 'searchCategories'])->name('buscador.searchCategories');
 
 
-//ruta crud categorias panel de  control
-route::resource('/categories', categoriaController::class)->names('categories');
 
 
-//buscador de productos panel de proveedores
-Route::get('/buscador_proveedores', [BuscadorController::class, 'searchproveedor'])->name('buscador.searchproveedor');
 
-//buscador de productos panel de Clientes
-Route::get('/buscador_clientes', [BuscadorController::class, 'searchclientes'])->name('buscador.searchclientes');
 
-//clientes crud
-Route::resource('/clientes', ClienteController::class)->names('clientes');
+
+
+
+
+
+
